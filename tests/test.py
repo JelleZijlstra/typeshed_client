fb8637e9fbf944b4a231e00c854ad63628100cc5
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2017-04-30 19:33:06 -0700

    add first tests

---
+++ tests/test.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+import typeshed_client
+from unittest import mock
+import unittest
+
+TEST_TYPESHED = Path(__file__).parent / 'typeshed'
+
+
+@mock.patch('typeshed_client.finder.find_typeshed', lambda: TEST_TYPESHED)
+class TestTypeshedClient(unittest.TestCase):
+    def test_get_stub_file(self) -> None:
+        self.assertEqual(typeshed_client.get_stub_file('lib'), TEST_TYPESHED / 'stdlib/3.5/lib.pyi')
+        self.assertEqual(typeshed_client.get_stub_file('lib', version=(3, 6)),
+                         TEST_TYPESHED / 'stdlib/3.5/lib.pyi')
+        self.assertEqual(typeshed_client.get_stub_file('lib', version=(3, 5)),
+                         TEST_TYPESHED / 'stdlib/3.5/lib.pyi')
+        self.assertEqual(typeshed_client.get_stub_file('lib', version=(3, 4)),
+                         TEST_TYPESHED / 'stdlib/3.4/lib.pyi')
+        self.assertEqual(typeshed_client.get_stub_file('lib', version=(3, 3)),
+                         TEST_TYPESHED / 'stdlib/3.3/lib.pyi')
+        self.assertEqual(typeshed_client.get_stub_file('lib', version=(3, 2)),
+                         TEST_TYPESHED / 'stdlib/3/lib.pyi')
+        self.assertEqual(typeshed_client.get_stub_file('lib', version=(2, 7)),
+                         TEST_TYPESHED / 'stdlib/2/lib.pyi')
+        for version in ((2, 7), (3, 3), (3, 4), (3, 5), (3, 6)):
+            self.assertEqual(typeshed_client.get_stub_file('shared', version=version),
+                             TEST_TYPESHED / 'stdlib/2and3/shared.pyi')
+
+
+if __name__ == '__main__':
+    unittest.main()
