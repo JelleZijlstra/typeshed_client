94b572bb47d851d58bef1d3edb1cd7262ff9aae7
Nicolas Ganz <gann@zhaw.ch>
2022-10-28 11:01:00 +0200

    explicitly re-export functions and types in the main package

--- typeshed_client/__init__.py
+++ typeshed_client/__init__.py
@@ -24,3 +24,22 @@ from .resolver import ImportedInfo, Resolver
 
 
 __version__ = "2.0.5"
+
+
+__all__ = [
+    "__version__",
+    "get_stub_ast",
+    "get_stub_file",
+    "get_all_stub_files",
+    "get_search_context",
+    "SearchContext",
+    "ModulePath",
+    "get_stub_names",
+    "parse_ast",
+    "ImportedName",
+    "NameDict",
+    "NameInfo",
+    "OverloadedName",
+    "ImportedInfo",
+    "Resolver",
+]
