d0c3e5ed85f9f556a882accdd04ad01c2d85d514
Gianfranco Costamagna <locutusofborg@debian.org>
2026-07-30 13:11:47 +0200

    Drop importlib_resources, use the new importlib.resources native Python3 class

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setup(
     },
     license="MIT",
     packages=["typeshed_client"],
-    install_requires=["importlib_resources >= 1.4.0", "typing-extensions>=4.5.0"],
+    install_requires=["typing-extensions>=4.5.0"],
     package_data={"typeshed_client": list(find_bundled_files())},
     classifiers=[
         "Development Status :: 3 - Alpha",
